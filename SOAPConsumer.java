36b8f28e5c1e5db5ebe03d4146fd34af46a41d1e
Nayan Wadekar <nayan.wadekar@gmail.com>
2016-09-26 09:08:47 +0100

    SOAP, JMS, REST service files added

---
+++ SOAPConsumer.java
@@ -0,0 +1,18 @@
+package com.consumer;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService(name = "SOAPService")
+public class SOAPConsumer{
+
+	@WebMethod
+	public String sanction(String input){		
+		return null;
+	}
+	
+	@WebMethod
+	public String rejection(String input){		
+		return null;
+	}
+}
