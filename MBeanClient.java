519eaea976b56aa9e1f8a4e3d00f6feb0951249d
Nayan Wadekar <nayan.wadekar@gmail.com>
2016-09-26 09:16:44 +0100

    JMX MBean & property reader.

---
+++ MBeanClient.java
@@ -0,0 +1,50 @@
+package util;
+
+import java.io.IOException;
+
+import javax.management.JMX;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+public class MBeanClient {
+
+	public static Class<?> classType;
+	
+	public <T> MBeanClient(Class<?> t) {
+		super();
+		classType = t;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> T fetchProxy(){
+		
+		try {
+			
+			ObjectName beanName = new ObjectName("com.jmx:type=IAppManagerMXBean");			
+			JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1617/jmxrmi");
+			JMXConnector jmxc = JMXConnectorFactory.connect(url);			
+			MBeanServerConnection jmxConn = jmxc.getMBeanServerConnection();						
+			T proxy =
+	                 (T) JMX.newMXBeanProxy(jmxConn,beanName,
+					classType, true);						
+			System.out.println("IS this registered - "+jmxConn.isRegistered(beanName));
+			
+//			System.out.println(MBeanUtils.createServer().isRegistered(beanName));
+			
+			return proxy;
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			
+		}
+		catch (MalformedObjectNameException ex) {
+			ex.printStackTrace();
+			// MalformedObjectNameException ex
+		}
+		return null;
+	}
+}
