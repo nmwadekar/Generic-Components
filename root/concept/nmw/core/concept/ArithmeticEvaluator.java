10a7a4f7387adbbf6da893a0b766919598f8f324
nayan.wadekar <nayan.wadekar@gmail.com>
2019-09-03 09:19:09 +0530

    Initial commit to Euler-68 problem

--- root/concept/nmw/core/concept/ArithmeticEvaluator.java
+++ root/concept/nmw/core/concept/ArithmeticEvaluator.java
@@ -1,284 +1,293 @@
 package nmw.core.concept;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class ArithmeticEvaluator {
 
-	private static final char[] OPERATORS = new char[] { '+', '-', '/', '*' };
-	
-	private static ScriptEngineManager mgr = new ScriptEngineManager();
-	private static ScriptEngine engine = mgr.getEngineByName("JavaScript");
-	
-	private static Map<String, ArrayList<Object>> mappings = new HashMap<String, ArrayList<Object>>();
+    private static final char[] OPERATORS = new char[]{'+', '-', '/', '*'};
 
-	public static void main(String[] args) throws ScriptException {
+    private static ScriptEngineManager mgr = new ScriptEngineManager();
+    private static ScriptEngine engine = mgr.getEngineByName("JavaScript");
 
-		String[] input = new String[]{"9","6","12", "8"};
-		
-		Set<String[]> a = evaluateExpression(input.length-1);
-		 
-		Combinator.resetOutput();
-		
-		Set<String[]> b =  evaluateOperands(input);
-		
-		formEquation(b, a);
-		
-		findEquation();
-		
-		System.out.println(mappings);
+    private static Map<String, ArrayList<Object>> mappings = new HashMap<String, ArrayList<Object>>();
 
-	}
+    public static void main(String[] args) throws ScriptException {
 
-	private static void findEquation() throws ScriptException{
-		
-		Set<String> eqs = getKeysByValue(mappings, new Integer(144));
-		
-		System.out.println("EQUATIONS FOUND FOR 144 - \n" + eqs);
-		
-		for(String eq : eqs){
-			
-			String equation = buildExpression(new String[]{"5", "40", "32", "12"}, new String[]{eq});
-			Object result = executeEquation(equation);
-			
-			BigDecimal digit_48 = new BigDecimal(48);
-			
-			BigDecimal bd = null;
-			
-			
-			if(result instanceof Integer){
-				bd = new BigDecimal((int)result);
-			}else{
-				bd = new BigDecimal((double)result);
-			}
-			
-			if(bd.equals(digit_48)){
-				
-				System.out.println("######### found formula = " + eq);
-			}
-			
-			System.out.println(result+ " type "  +result.getClass());
-		}
-		
-		
-	}
-	
-	private static String[] convert(Set<String> input){
-		
-		String[] output = new String[input.size()];
-		
-		int i = 0;
-		
-		for(String s : input){
-			
-			output[i++] = s;
-		}
-		
-		return output;
-	}
-	
-	public static <T, E> Set<T> getKeysByValue(Map<T, ArrayList<E>> map, E value) {
-	    Set<T> keys = new HashSet<T>();
-	    for (Entry<T, ArrayList<E>> entry : map.entrySet()) {
-	        if (entry.getValue().contains(value)) {
-	            keys.add(entry.getKey());
-	        }
-	    }
-	    return keys;
-	}
-	
-	private static void formEquation(Set<String[]> operands, Set<String[]> expressions) throws ScriptException{
-		
-		String equation = null;
-		Object result = null;
-		
-		for(String[] o : operands){
-			
-			for(String[] e : expressions){
-				
-				equation = buildExpression(o, e);
-				result = executeEquation(equation);
-				
-				if(mappings.containsKey(new String(e[0]))){
-				
-					ArrayList<Object> l = mappings.get(new String(e[0]));
-					
-					l.add(result);
-					
-					mappings.put(new String(e[0]), l);
-					
-				}else{
-				
-					ArrayList<Object> a = new ArrayList<>();
-					a.add(result);
-					
-					mappings.put(new String(e[0]), a);
-				}
-				
-				System.out.println(new String(e[0]) + "#" + result);
-				
-				System.out.println("EQUATION ["+equation + "] = " + result /*+ " TYPE = " + result.getClass()*/);
-				
-			}
-		}
-	}
+        String[] input = new String[]{"9", "6", "12", "8"};
+
+        Set<String[]> a = evaluateExpression(input.length - 1);
+
+        Combinator.resetOutput();
+
+        Set<String[]> b = evaluateOperands(input);
+
+        formEquation(b, a);
+
+        findEquation();
+
+        System.out.println(mappings);
+    }
+
+    private static void findEquation() throws ScriptException {
+
+        Set<String> eqs = getKeysByValue(mappings, new Integer(144));
+
+        System.out.println("EQUATIONS FOUND FOR 144 - \n" + eqs);
+
+        for (String eq : eqs) {
+
+            String equation = buildExpression(new String[]{"5", "40", "32", "12"}, new String[]{eq});
+            Object result = executeEquation(equation);
+
+            BigDecimal digit_48 = new BigDecimal(48);
+
+            BigDecimal bd = null;
+
+            if (result instanceof Integer) {
+                bd = new BigDecimal((int) result);
+            } else {
+                bd = new BigDecimal((double) result);
+            }
+
+            if (bd.equals(digit_48)) {
+
+                System.out.println("######### found formula = " + eq);
+            }
+
+            System.out.println(result + " type " + result.getClass());
+        }
+    }
+
+    private static String[] convert(Set<String> input) {
+
+        String[] output = new String[input.size()];
+
+        int i = 0;
+
+        for (String s : input) {
+
+            output[i++] = s;
+        }
+
+        return output;
+    }
+
+    public static <T, E> Set<T> getKeysByValue(Map<T, ArrayList<E>> map, E value) {
+        Set<T> keys = new HashSet<T>();
+        for (Entry<T, ArrayList<E>> entry : map.entrySet()) {
+            if (entry.getValue().contains(value)) {
+                keys.add(entry.getKey());
+            }
+        }
+        return keys;
+    }
+
+    private static void formEquation(Set<String[]> operands, Set<String[]> expressions)
+            throws ScriptException {
+
+        String equation = null;
+        Object result = null;
+
+        for (String[] o : operands) {
+
+            for (String[] e : expressions) {
+
+                equation = buildExpression(o, e);
+                result = executeEquation(equation);
+
+                if (mappings.containsKey(new String(e[0]))) {
+
+                    ArrayList<Object> l = mappings.get(new String(e[0]));
+
+                    l.add(result);
+
+                    mappings.put(new String(e[0]), l);
+
+                } else {
+
+                    ArrayList<Object> a = new ArrayList<>();
+                    a.add(result);
+
+                    mappings.put(new String(e[0]), a);
+                }
+
+                System.out.println(new String(e[0]) + "#" + result);
+
+                System.out.println(
+                        "EQUATION [" + equation + "] = " + result /*+ " TYPE = " + result.getClass()*/);
+            }
+        }
+    }
+
+    public static Set<String> roundRobin(int n, int r, char[] input) {
+
+        for (int i = 0; i < input.length; i++) {
+
+            for (int j = 0; j < r; j++) {
+
+                System.out.print(input[(i + j) % n]);
+            }
+
+            System.out.println();
+        }
+        return null;
+    }
+
+    public static char[] fill(char c, int n) {
+
+        char[] output = new char[n];
+
+        for (int i = 0; i < n; i++) {
+
+            output[i] = c;
+        }
+
+        return output;
+    }
+
+    public static List<char[]> alterPivot(char pivot, char[] input) {
+
+        List<char[]> output = new ArrayList<char[]>();
+
+        for (int i = 0; i < input.length; i++) {
+
+            char[] o = Arrays.copyOf(input, input.length);
+
+            o[i] = pivot;
+
+            output.add(o);
+        }
+
+        return output;
+    }
+
+    public static Set<String[]> evaluateExpression(int count) {
+
+        Set<String[]> expressionPossibility = new HashSet<>();
+
+        for (int i = 0; i < OPERATORS.length; i++) {
+
+            char[] expression = fill(OPERATORS[i], count);
+
+            for (int j = 0; j < OPERATORS.length /* && i!=j */; j++) {
+
+                List<char[]> pivoted = alterPivot(OPERATORS[j], expression);
+
+                for (String[] p : convert(pivoted)) {
+
+                    expressionPossibility.addAll(Combinator.find(0, p));
+                }
+            }
+        }
+
+        return expressionPossibility;
+    }
+
+    private static List<String[]> convert(List<char[]> input) {
+
+        List<String[]> output = new ArrayList<>();
+
+        for (char[] c : input) {
+
+            String s[] = new String[]{new String(c)};
+
+            output.add(s);
+        }
+
+        return output;
+    }
+
+    public static Set<String[]> evaluateOperands(String[] input) {
+
+        Set<String[]> expressionPossibility = new HashSet<>();
+
+        expressionPossibility.addAll(Combinator.find(0, input));
+
+        return expressionPossibility;
+    }
+
+    public static String buildExpression(String[] input, String[] operator) {
+
+        StringBuilder sb = new StringBuilder();
+
+        char[] exp = operator[0].toCharArray();
+
+        for (int i = 0; i < input.length - 1; i++) {
+
+            sb.append(input[i]);
+            sb.append(exp[i]);
+        }
+
+        sb.append(input[input.length - 1]);
+
+        return sb.toString();
+    }
+
+    public static Object executeEquation(String eq) throws ScriptException {
+
+        return engine.eval(eq);
+    }
+
+  public static void print(char[] input) {
+
+    for (int i = 0; i < input.length; i++) {
+
+      System.out.print(input[i]);
+    }
+  }
+
+  public static void print(int[] input) {
+
+    for (int i = 0; i < input.length; i++) {
+
+      System.out.print(input[i]);
+    }
+  }
 
 
-	public static Set<String> roundRobin(int n, int r, char[] input) {
+  public static void print(List<char[]> input) {
 
-		for (int i = 0; i < input.length; i++) {
+        for (char[] i : input) {
 
-			for (int j = 0; j < r; j++) {
+            print(i);
+        }
+    }
 
-				System.out.print(input[(i + j) % n]);
-			}
+    public static void printx(Set<?> input) {
 
-			System.out.println();
-		}
-		return null;
-	}
+        List<String[]> sorted = new ArrayList<String[]>((Set<String[]>)input);
 
-	public static char[] fill(char c, int n) {
+        for (Object i : sorted) {
 
-		char[] output = new char[n];
+            printx((String[])i);
+        }
 
-		for (int i = 0; i < n; i++) {
+        System.out.println();
+    }
 
-			output[i] = c;
-		}
 
-		return output;
-	}
+  public static void  printInt(Set<int[]> input) {
 
-	public static List<char[]> alterPivot(char pivot, char[] input) {
+    for (int[] i : input) {
 
-		List<char[]> output = new ArrayList<char[]>();
+      print(i);
+    }
 
-		for (int i = 0; i < input.length; i++) {
+    System.out.println();
+  }
 
-			char[] o = Arrays.copyOf(input, input.length);
+    private static void printx(String[] i) {
 
-			o[i] = pivot;
+        for (String s : i) {
 
-			output.add(o);
-		}
+            System.out.print(s + "\t");
+        }
 
-		return output;
-	}
-
-	public static Set<String[]> evaluateExpression(int count) {
-
-		Set<String[]> expressionPossibility = new HashSet<>();
-
-		for (int i = 0; i < OPERATORS.length; i++) {
-
-			char[] expression = fill(OPERATORS[i], count);
-
-			for (int j = 0; j < OPERATORS.length/* && i!=j */; j++) {
-
-				List<char[]> pivoted = alterPivot(OPERATORS[j], expression);
-
-				for (String[] p : convert(pivoted)) {
-
-					expressionPossibility.addAll(Combinator.find(0, p));
-				}
-			}
-		}
-		
-		return expressionPossibility;
-	}
-	
-	private static List<String[]> convert(List<char[]> input){
-		
-		List<String[]> output = new ArrayList<>();
-		
-		for(char[] c : input){
-			
-			String s[] = new String[]{new String(c)};
-			
-			output.add(s);
-		}
-		
-		return output;
-	}
-	
-	public static Set<String[]> evaluateOperands(String[] input) {
-
-		Set<String[]> expressionPossibility = new HashSet<>();
-
-		expressionPossibility.addAll(Combinator.find(0, input));
-		
-		return expressionPossibility;
-	}
-
-	public static String buildExpression(String[] input, String[] operator) {
-
-		StringBuilder sb = new StringBuilder();
-
-		char[] exp = operator[0].toCharArray();
-		
-		for (int i = 0; i < input.length - 1; i++) {
-
-			sb.append(input[i]);
-			sb.append(exp[i]);
-		}
-
-		sb.append(input[input.length - 1]);
-		
-		return sb.toString();
-	}
-	
-	public static Object executeEquation(String eq) throws ScriptException {
-
-		return engine.eval(eq);
-	}
-
-	public static void print(char[] input) {
-
-		for (int i = 0; i < input.length; i++) {
-
-			System.out.print(input[i]);
-		}
-	}
-
-	public static void print(List<char[]> input) {
-
-		for (char[] i : input) {
-
-			print(i);
-		}
-	}
-	
-	public static void printx(Set<String[]> input) {
-
-		for (String[] i : input) {
-
-			printx(i);	
-		}
-		
-		System.out.println();
-	}
-	
-	private static void printx(String[] i){
-		
-		for(String s : i){
-			
-			System.out.print(s + "\t");
-		}
-		
-		System.out.println();
-	}
-	
-		
+        System.out.println();
+    }
 }
---
+++ root/concept/nmw/core/concept/MagicFiveGonRing.java
@@ -0,0 +1,220 @@
+package nmw.core.concept;
+
+import java.util.*;
+
+public class MagicFiveGonRing {
+
+    private static Combinator combinor = new Combinator();
+
+    public static void main(String[] args) {
+
+//        List<int[]> intComb = null;
+//        Set<String[]> combinations = null;
+
+        Set<String[]> combinations = combinor.find(0, new String[]{"1", "2", "3", "4", "5", "6"});
+
+        combinor.resetOutput();
+
+//        ArithmeticEvaluator.printx(combinations);
+
+        List<int[]> intComb = convert(combinations);
+
+//        formThreeRing(intComb);
+
+        processThreeRing(combinations);
+
+        //--     FOR 5 RING
+        /*
+        combinations = combinor.find(0, new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"});
+
+        ArithmeticEvaluator.printx(combinations);
+
+        intComb = convert(combinations);
+
+        formFiveRing(intComb);
+        */
+    }
+
+    private static void formThreeRing(List<int[]> input) {
+
+        String format = "%d %d %d, %d %d %d, %d %d %d";
+
+        for (int[] i : input) {
+
+            if ((i[0] + i[1] + i[2]) == (i[3] + i[2] + i[4]) && (i[3] + i[2] + i[4]) == i[5] + i[4] + i[1]) {
+
+                System.out.println(String.format(format, i[0], i[1], i[2], i[3], i[2], i[4], i[5], i[4], i[1]));
+
+            }
+        }
+    }
+
+    private static void processThreeRing(Set<String[]> input) {
+
+//        String format = "%d %d %d, %d %d %d, %d %d %d";
+        String format = "%s, %s, %s";
+
+        String[] a, b, c;
+
+        for (String[] i : input) {
+
+            a = new String[]{i[0], i[1], i[2]};
+            b = new String[]{i[3], i[2], i[4]};
+            c = new String[]{i[5], i[4], i[1]};
+
+            if ((i[0] + i[1] + i[2]) == (i[3] + i[2] + i[4]) && (i[3] + i[2] + i[4]) == i[5] + i[4] + i[1]) {
+
+//                System.out.println(String.format(format, i[0], i[1], i[2], i[3], i[2], i[4], i[5], i[4], i[1]));
+
+                System.out.println("GOT iT = " + String.format(format, a, b, c));
+
+            }
+
+            permuteThree(a, b, c);
+        }
+    }
+
+    private static Set<String[]> permuteThree(String[]... input) {
+
+        Set<String[]> resultant = new HashSet<>();
+        String[] tempo;
+
+        String[] a = input[0];
+        String[] b = input[1];
+        String[] c = input[2];
+
+        int[] ia = convert(a);
+        int[] ib = convert(b);
+        int[] ic = convert(c);
+
+
+/*        if ((ia[0] + ia[1] + ia[2]) == (ib[0] + ib[1] + ib[2]) && (ib[0] + ib[1] + ib[2]) == ic[0] + ic[1] + ic[2]) {
+
+//                System.out.println(String.format(format, i[0], i[1], i[2], i[3], i[2], i[4], i[5], i[4], i[1]));
+
+//            System.out.println("GOT iT = "+ String.format("%s%s%s",convert(a),convert(b),convert(c)));
+
+            ArithmeticEvaluator.print(convert(a));
+            ArithmeticEvaluator.print(convert(b));
+            ArithmeticEvaluator.print(convert(c));
+        }*/
+
+        List<String[]> aP = new ArrayList(combinor.find(0, a));
+        combinor.resetOutput();
+
+        List<String[]> bP = new ArrayList(combinor.find(0, b));
+        combinor.resetOutput();
+
+        List<String[]> cP = new ArrayList(combinor.find(0, c));
+        combinor.resetOutput();
+
+        for (int i = 0; i < aP.size(); i++) {
+
+            for (int j = 0; j < bP.size(); j++) {
+
+                for (int k = 0; k < cP.size(); k++) {
+
+                    if( ((Integer.valueOf(aP.get(i)[0]) + Integer.valueOf(aP.get(i)[1]) + Integer.valueOf(aP.get(i)[2])) ==
+                            ((Integer.valueOf(bP.get(i)[0]) + Integer.valueOf(bP.get(i)[1]) + Integer.valueOf(bP.get(i)[2])))) &&
+                            ((Integer.valueOf(cP.get(i)[0]) + Integer.valueOf(cP.get(i)[1]) + Integer.valueOf(cP.get(i)[2])) ==
+                                    ((Integer.valueOf(bP.get(i)[0]) + Integer.valueOf(bP.get(i)[1]) + Integer.valueOf(bP.get(i)[2]))))
+                                         ) {
+
+
+
+
+                        tempo = combine(aP.get(i), bP.get(j), cP.get(k));
+
+                        /*if ((tempo[0] +tempo[1] + tempo[2]) == (tempo[3] + tempo[4] + tempo[5]) &&
+                                (tempo[3] + tempo[4] + tempo[5]) == tempo[6] + tempo[7] + tempo[8]){
+*/
+                            resultant.add(tempo);
+//                        }
+
+
+//                System.out.println(String.format(format, i[0], i[1], i[2], i[3], i[2], i[4], i[5], i[4], i[1]));
+                    }
+
+                }
+            }
+
+        }
+
+        ArithmeticEvaluator.printx(resultant);
+
+        return resultant;
+    }
+
+    private static String[] combine(String[]... input) {
+
+        String[] tempo, output = new String[input.length * input[0].length];
+
+        int n = 0;
+
+        for (int i = 0; i < input.length; i++) {
+
+            tempo = input[i];
+
+            for (int j = 0; j < tempo.length; j++) {
+
+                output[n++] = tempo[j];
+            }
+        }
+
+        return output;
+    }
+
+
+    private static int[] convert(String[] input) {
+
+        int[] output = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+
+            output[i] = Integer.valueOf(input[i]);
+        }
+
+        return output;
+    }
+
+
+    private static void formFiveRing(List<int[]> input) {
+
+        String format = "%d %d %d,%d %d %d,%d %d %d,%d %d %d,%d %d %d";
+
+        for (int[] i : input) {
+
+//            ArithmeticEvaluator.print(i);
+
+            int a = i[0] + i[1] + i[2];
+            int b = i[3] + i[2] + i[4];
+            int c = i[5] + i[4] + i[6];
+            int d = i[7] + i[6] + i[8];
+            int e = i[9] + i[8] + i[1];
+
+            if ((a == b) && (b == c) && (c == d) && (d == e)) {
+
+                System.out.println(String.format(format,
+                        i[0], i[1], i[2],
+                        i[3], i[2], i[4],
+                        i[5], i[4], i[6],
+                        i[7], i[6], i[8],
+                        i[9], i[8], i[1]));
+
+            }
+        }
+    }
+
+
+    private static List<int[]> convert(Set<String[]> input) {
+
+        List<int[]> output = new ArrayList<>(input.size());
+
+        for (String[] s : input) {
+
+            output.add(convert(s));
+        }
+
+        return output;
+    }
+}
